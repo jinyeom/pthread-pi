8dad93f35618521ea8f7be9d98c7dc31f2c8227b
jinyeom <jinyeom95@gmail.com>
2018-04-14 18:20:57 -0500

    progress

--- pi_plot.py
+++ pi_plot.py
@@ -3,7 +3,7 @@ import subprocess
 import numpy as np
 from matplotlib import pyplot as plt
 
-def plot_nthreads_time(prog, title, plot_show=False, plot_save=False, table_show=False, table_save=False):
+def plot_nthreads_time(prog, title, show=False, save=False):
     # plot the naive pthread implemenation of computation of pi with 1, 2, 4, 8 threads
     print("Plotting runtimes of {} with 1, 2, 4, 8 threads...".format(prog))
     times = []
@@ -24,11 +24,11 @@ def plot_nthreads_time(prog, title, plot_show=False, plot_save=False, table_show
     plt.xlabel("number of threads")
     plt.ylabel("nanoseconds")
     plt.plot(n_threads, times, marker="o")
-    if plot_show:
+    if show:
         print("Displaying the plot...")
         plt.show()
-    if plot_save:
-        print("Saving figure...", flush=True, end="")
+    if save:
+        print("Saving plot figure...", flush=True, end="")
         plt.savefig("{}.png".format(title))
         print("\x1B[32mdone\x1B[0m")
         plt.close()
@@ -39,8 +39,15 @@ def plot_nthreads_time(prog, title, plot_show=False, plot_save=False, table_show
                     cellLoc="center")
     plt.title(title)
     plt.axis("off")
-    plt.show()
+    if show:
+        print("Displaying the table...")
+        plt.show()
+    if save:
+        print("Saving table figure...", flush=True, end="")
+        plt.savefig("{}_table.png".format(title))
+        print("\x1B[32mdone\x1B[0m")
+        plt.close()
 
-plot_nthreads_time("./pi_naive", "Naive Parallelization with Pthread", plot_save=True)
-plot_nthreads_time("./pi_mutex", "Pthread Parallelization with Mutex", plot_save=True)
-plot_nthreads_time("./pi_atomic", "Pthread Parallelization with Atomic Instructions", plot_save=True)
+plot_nthreads_time("./pi_naive", "Naive Parallelization with Pthread", save=True)
+plot_nthreads_time("./pi_mutex", "Pthread Parallelization with Mutex", save=True)
+plot_nthreads_time("./pi_atomic", "Pthread Parallelization with Atomic Instructions", save=True)
