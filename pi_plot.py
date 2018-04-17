bb9d705227c6e8f3d63b09361b73eec3779b31c0
jinyeom <jinyeom95@gmail.com>
2018-04-17 15:31:07 -0500

    progress

--- pi_plot.py
+++ pi_plot.py
@@ -3,10 +3,16 @@ import subprocess
 import numpy as np
 from matplotlib import pyplot as plt
 
+print("Running {} in serial...".format(prog), flush=True, end="")
+result = subprocess.run([prog, str(n)], stdout=subprocess.PIPE).stdout.decode("utf-8")
+print("\x1B[32mdone\x1B[0m")
+serial_runtime = float(re.search(r"elapsed process CPU time = (.*) nanoseconds", result).group(1))
+
 def plot_nthreads_time(prog, title, show=False, save=False):
     # plot the naive pthread implemenation of computation of pi with 1, 2, 4, 8 threads
     print("Plotting runtimes of {} with 1, 2, 4, 8 threads...".format(prog))
-    times = []
+    runtimes = []
+    speedups = []
     pi_vals = []
     n_threads = [1, 2, 4, 8]
     for n in n_threads:
@@ -14,15 +20,30 @@ def plot_nthreads_time(prog, title, show=False, save=False):
         result = subprocess.run([prog, str(n)], stdout=subprocess.PIPE).stdout.decode("utf-8")
         print("\x1B[32mdone\x1B[0m")
         # first line of the result should have the runtime
-        t = re.search(r"elapsed process CPU time = (.*) nanoseconds", result)
-        times.append(float(t.group(1)))
+        parallel_runtime = float(re.search(r"elapsed process CPU time = (.*) nanoseconds", result).group(1))
+        runtimes.append(parallel_runtime)
+        speedups.append(serial_runtime / parallel_runtime)
         # second line should have the value of pi computed
         pi_vals.append(float(result.split("\n")[1]))
 
     plt.figure()
     plt.title(title)
     plt.xlabel("number of threads")
-    plt.ylabel("nanoseconds")
+    plt.ylabel("speedup")
+    plt.plot(n_threads, speedups, marker="o")
+    if show:
+        print("Displaying the plot...")
+        plt.show()
+    if save:
+        print("Saving plot figure...", flush=True, end="")
+        plt.savefig("{}.png".format(title))
+        print("\x1B[32mdone\x1B[0m")
+        plt.close()
+    
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("number of threads")
+    plt.ylabel("runtime (nanoseconds)")
     plt.plot(n_threads, times, marker="o")
     if show:
         print("Displaying the plot...")
